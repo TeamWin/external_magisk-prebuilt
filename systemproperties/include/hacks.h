ac5e5bb9077423c8f93c20784f671572b3f08b50
Chaosmaster <chaosmaster@chaosv.de>
2020-02-06 00:29:19 +0100

    libresetprop: Fix compilation in 8.1
    Change-Id: I7f8d86659116efd4a816045dc028c4b801a6077e

--- systemproperties/include/hacks.h
+++ systemproperties/include/hacks.h
@@ -12,3 +12,7 @@
 #define getline __getline
 #define fsetxattr(...) syscall(__NR_fsetxattr, __VA_ARGS__)
 ssize_t __getline(char **, size_t *, FILE *);
+
+#ifndef __BIONIC_ALIGN
+#define __BIONIC_ALIGN(__value, __alignment) (((__value) + (__alignment)-1) & ~((__alignment)-1))
+#endif
